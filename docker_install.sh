83aa5d62cc6bfbe0e2020c66a26ad06885f3ac21
ainsoflv <38898166+ainsoflv@users.noreply.github.com>
2022-01-14 19:35:44 +0800

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 stty erase ^H
 PORT=0
-
+#判断当前端口是否被占用，没被占用返回0，反之1
+function Listening() {
+  TCPListeningnum=$(netstat -an | grep ":$1 " | awk '$1 == "tcp" && $NF == "LISTEN" {print $0}' | wc -l)
+  UDPListeningnum=$(netstat -an | grep ":$1 " | awk '$1 == "udp" && $NF == "0.0.0.0:*" {print $0}' | wc -l)
+  ((Listeningnum = TCPListeningnum + UDPListeningnum))
+  if [ $Listeningnum == 0 ]; then
+    echo "0"
+  else
+    echo "1"
+  fi
+}
 
 case $(uname -m) in
 x86_64) is_x86=1 ;;
@@ -251,3 +261,196 @@ EOF
   fi
   rm -fr build.log
 fi
+
+dir='jd-qinglong'
+TIME y "请指定保存数据的目录，已存在的请指定名字，回车默认jd-qinglong"
+read input
+if [ -z "${input}" ]; then
+  input=$dir
+fi
+dir=$input
+
+if [ ! -d $dir ]; then
+  mkdir $dir
+fi
+
+cd $dir || exit
+
+rm -f adbot/adbot
+
+file=env.properties
+if [ ! -f "$file" ]; then
+  wget -O env.properties https://ghproxy.com/https://raw.githubusercontent.com/rubyangxg/jd-qinglong/master/env.template.properties
+else
+  echo "env.properties已存在"
+fi
+
+sudo docker rm -f webapp
+if [ $is_x86 == 1 ]; then
+  sudo docker pull rubyangxg/jd-qinglong
+else
+  sudo docker pull rubyangxg/jd-qinglong:arm
+fi
+
+ad_port1=5701
+TIME y "请设置阿东网页登录端口：(数字80~65535)，回车默认5701"
+while [ 1 ]; do
+  read input
+  if [ -z "${input}" ]; then
+    input=5701
+  fi
+  if [ $input -gt 79 -a $input -lt 65536 ]; then
+    grep_port=$(netstat -tlpn | grep "\b$input\b")
+    if [ -n "$grep_port" ]; then
+      get_random_port 5701 5800
+      ad_port1=$PORT
+      echo -e "端口 $input 已被占用，生成随机端口$ad_port1，配置成功\n"
+    else
+      echo -e "端口 $input 未被使用，配置成功\n"
+      ad_port1=$input
+    fi
+    break
+  else
+    TIME r "别瞎搞，请输入端口：(数字80~65535)"
+  fi
+done
+
+ad_port2=9527
+grep_port=$(netstat -tlpn | grep "\b$ad_port2\b")
+if [ -n "$grep_port" ]; then
+  get_random_port 9527 9600
+  ad_port2=$PORT
+  TIME -e "端口 9527 已被占用，生成随机端口$ad_port2，配置成功\n"
+fi
+
+
+adbotDir="$(pwd)"/adbot
+if [ $synology == 1 ]; then
+  if [ ! -d $adbotDir ]; then
+    mkdir $adbotDir
+  fi
+fi
+
+if [ $is_x86 == 1 ]; then
+  docker run -d -p $ad_port1:8080 -p $ad_port2:8090 --name=webapp --privileged=true -v "$(pwd)"/env.properties:/env.properties:rw -v "$(pwd)"/adbot:/adbot rubyangxg/jd-qinglong
+else
+  docker run -d -p $ad_port1:8080 -p $ad_port2:8090 --name=webapp  -e "SPRING_PROFILES_ACTIVE=arm" --privileged=true -v "$(pwd)"/env.properties:/env.properties:rw -v "$(pwd)"/adbot:/adbot rubyangxg/jd-qinglong:arm
+fi
+
+while [ 1 ]; do
+  if [ -f "./adbot/adbot" ]; then
+    sleep 1s
+    echo "阿东启动成功"
+    break
+  else
+    echo "等待阿东启动完成，生成必要文件"
+    sleep 1s
+  fi
+done
+
+json='{"server_groups":[{"name":"webapp","disabled":false,"json":false,"urls":["ws://localhost:'$ad_port1'/ws/cq/"],"event_filter":[],"regex_filter":"","regex_replace":"","extra_header":{"User-Agent":["GMC"]}},{"name":"webapp_admin","disabled":false,"json":false,"urls":["ws://localhost:'$ad_port2'/ws/cq/"],"event_filter":[],"regex_filter":"","regex_replace":"","extra_header":{"User-Agent":["GMC"]}}]}'
+echo $json >./adbot/gmc_config.json
+
+cd adbot || exit
+chmod +x adbot
+
+echo "请创建一个机器人管理页面用户名：(字母数字下划线)，回车默认admin"
+while [ 1 ]; do
+  read input
+  if [ -z "${input}" ]; then
+    input="admin"
+  fi
+  if [[ $input =~ ^[A-Za-z0-9_]+$ ]]; then
+    username=$input
+    break
+  else
+    echo "别瞎搞，请输入用户名：(字母数字下划线)，回车默认随机字符"
+  fi
+done
+
+echo "请设置机器人管理页面密码：(字母数字下划线)，回车默认adbotadmin"
+while [ 1 ]; do
+  read input
+  if [ -z "${input}" ]; then
+    input="adbotadmin"
+  fi
+  if [[ $input =~ ^[A-Za-z0-9_]+$ ]]; then
+    password=$input
+    break
+  else
+    echo "别瞎搞，请输入密码：(字母数字下划线)，回车默认随机字符"
+  fi
+done
+
+killall adbot
+
+port=8100
+echo "请设置机器人管理页面登录端口：(数字80~65535)，回车默认8100"
+while [ 1 ]; do
+  read input
+  if [ -z "${input}" ]; then
+    input=8100
+  fi
+  if [ $input -gt 79 -a $input -lt 65536 ]; then
+    grep_port=$(netstat -tlpn | grep "\b$input\b")
+    if [ -n "$grep_port" ]; then
+      get_random_port 8100 8200
+      port=$PORT
+      echo -e "端口 $input 已被占用，生成随机端口$port，配置成功\n"
+    else
+      echo -e "端口 $input 未被使用，配置成功\n"
+      port=$input
+    fi
+    break
+  else
+    echo "别瞎搞，请输入端口：(数字80~65535)"
+  fi
+done
+
+echo "机器人管理页面用户名是$username"
+echo "机器人管理页面密码是$password"
+echo "机器人管理页面端口是$port"
+echo "阿东网页登录端口$ad_port1"
+
+sed -i "s#^username=.*#username=$username#g" ./start-adbot.sh
+sed -i "s#^password=.*#password=$password#g" ./start-adbot.sh
+sed -i "s#^port=.*#port=$port#g" ./start-adbot.sh
+
+chmod +x ./start-adbot.sh
+bash ./start-adbot.sh restart
+
+hasError1=1
+for i in {1..10}; do
+  urlstatus=$(curl -s -m 5 -IL http://localhost:$ad_port1/ping | grep 200)
+  if [ "$urlstatus" == "" ]; then
+    echo "检查是否可访问阿东页面...第 $i 次(共10次)，如果机器太慢可能会检查超时，请自行确认服务正常。"
+    sleep 5s
+  else
+    hasError1=0
+    break
+  fi
+done
+
+hasError2=1
+for i in {1..10}; do
+  urlstatus=$(curl -u $username:$password -s -m 5 -IL http://localhost:$port | grep 200)
+  if [ "$urlstatus" == "" ]; then
+    echo "检查是否可访问机器人管理页面...第 $i 次(共10次)"
+    sleep 6s
+  else
+    hasError2=0
+    break
+  fi
+done
+
+if [ $hasError1 == 1 -o $hasError2 == 1 ]; then
+  echo "出错了，请联系作者，查看日志docker logs -f webapp"
+else
+  sed -i '/^ADONG.URL.*/d' ../env.properties
+  sed -i '$aADONG.URL=http://localhost:'$ad_port1'' ../env.properties
+  echo "恭喜你安装完成，阿东网页：http://localhost:$ad_port1，阿东机器人登录入口：http://localhost:$port，外部访问请打开防火墙并且开放 $ad_port1 和 $port 端口！"
+fi
+
+#bash <(curl -s -L https://ghproxy.com/https://raw.githubusercontent.com/rubyangxg/jd-qinglong/master/install.sh)
+#sed -e '0,/localhost:[0-9]\+/ s/localhost:[0-9]\+/localhost:1245/' ./adbot/gmc_config.json
+#tac ./adbot/gmc_config.json | sed -e '0,/localhost:[0-9]\+/{s/localhost:[0-9]\+/localhost:1245/}' | tac | tee a.json
