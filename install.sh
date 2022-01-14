424e80dd4ea20c38123ae593eceaed70b0ce1044
ainsoflv <38898166+ainsoflv@users.noreply.github.com>
2022-01-14 20:08:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 stty erase ^H
 PORT=0
 #判断当前端口是否被占用，没被占用返回0，反之1
-function Listening() {
+Listening() {
   TCPListeningnum=$(netstat -an | grep ":$1 " | awk '$1 == "tcp" && $NF == "LISTEN" {print $0}' | wc -l)
   UDPListeningnum=$(netstat -an | grep ":$1 " | awk '$1 == "udp" && $NF == "0.0.0.0:*" {print $0}' | wc -l)
   ((Listeningnum = TCPListeningnum + UDPListeningnum))
